43280828a8a309b9eb89371fb48ad9cd7b225181
Cloudyheaveen <abirarahou2002@gmail.com>
2023-04-19 14:53:38 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Check the code
+ * @argc: The number of arguments
+ * @argv: An array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	op = argv[2];
+	b = atoi(argv[3]);
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
