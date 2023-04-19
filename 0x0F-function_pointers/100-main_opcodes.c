48488d705aaeda243e5e69c6f4ae69ebc912a2a2
Cloudyheaveen <abirarahou2002@gmail.com>
2023-04-19 15:18:32 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -26,12 +26,10 @@ int main(int argc, char *argv[])
 	arr = (char *)main;
 	for (i = 0; i < num_bytes; i++)
 	{
-		if (i == num_bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx\n", arr[i]);
+		printf("%02hhx", arr[i]);
+		if (i != num_bytes - 1)
+			printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
