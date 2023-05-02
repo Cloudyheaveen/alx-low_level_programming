3ecdd572c94abaee0c249792b7c5147ae707c38f
Cloudyheaveen <abirarahou2002@gmail.com>
2023-05-02 10:11:25 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,34 +10,38 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 0;
-	const listint_t *first, *last;
+	const listint_t *first =  head, *last = head;
 
-	first = head;
-	last = head;
-
-	while (first != NULL && last != NULL && last->next != NULL)
+	if (head == NULL)
+		return (0);
+	while (last != NULL && last->next != NULL)
 	{
-		printf("[%p] %d\n", (void *) first, first->n);
+		printf("[%p] %d\n", (void *)first, first->n);
+		i++;
 		first = first->next;
 		last = last->next->next;
-		i++;
 		if (first == last)
-		{
-			printf("[%p] %d\n", (void *) first, first->n);
-			printf("-> [%p] %d\n", (void *) last->next, last->next->n);
-			i++;
 			break;
+	}
+	if (last == NULL || last->next == NULL)
+		while (first != NULL)
+		{
+			printf("[%p] %d\n", (void *)first, first->n);
+			i++;
+			first = first->next;
 		}
-	}
-	while (first != NULL)
+	else
 	{
-		printf("[%p] %d\n", (void *) first, first->n);
-		first = first->next;
+		first = head;
+		while (first != last)
+		{
+			printf("[%p] %d\n", (void *)first, first->n);
+			i++;
+			first = first->next;
+			last = last->next;
+		}
+		printf("[%p] %d\n", (void *)first, first->n);
 		i++;
-	}
-	if (head == NULL || i == 0)
-	{
-		printf("-> [NULL]\n");
 	}
 	return (i);
 }
